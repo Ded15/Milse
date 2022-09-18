a41959aaa0122800c6d70abc42caf2d2c4dfb2fe
Julia Hardikova <iylia.hardikova@mail.ru>
2022-09-18 19:00:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int tickets = 10_000; // Стоимоть билета
+
+        int milse = 20; // Условие для начисление 1 бонуса
+
+        System.out.println("Начисленные бонусы:" + (tickets / milse));
+
+    }
+}
